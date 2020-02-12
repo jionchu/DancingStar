1fa596594167476b3e590905d56cb112ed7b79c6
jionchu <fn0809@gmail.com>
2020-02-12 11:46:02 +0900

    Play VideoView

--- Android/DancingStar/app/src/main/java/org/evolution/dancingstar/dance/DanceActivity.java
+++ Android/DancingStar/app/src/main/java/org/evolution/dancingstar/dance/DanceActivity.java
@@ -1,15 +1,40 @@
 package org.evolution.dancingstar.dance;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 import org.evolution.dancingstar.R;
 
 public class DanceActivity extends AppCompatActivity {
 
+    private VideoView mVideoView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dance);
+
+        mVideoView = findViewById(R.id.dance_video_view);
+
+        TextView tvUser = findViewById(R.id.dance_tv_user);
+        tvUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playVideo();
+            }
+        });
+    }
+
+    public void playVideo() {
+        String path = "android.resource://org.evolution.dancingstar/"+R.raw.dance_example;
+
+        Uri uri = Uri.parse(path);
+
+        mVideoView.setVideoURI(uri);
+        mVideoView.start();
     }
 }
--- Android/DancingStar/app/src/main/java/org/evolution/dancingstar/learn/LearnActivity.java
+++ Android/DancingStar/app/src/main/java/org/evolution/dancingstar/learn/LearnActivity.java
@@ -1,15 +1,83 @@
 package org.evolution.dancingstar.learn;
 
+import android.hardware.Camera;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 import org.evolution.dancingstar.R;
 
 public class LearnActivity extends AppCompatActivity {
 
+    private Camera camera;
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+    private boolean recording = false;
+    private VideoView mVideoView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn);
+
+        mVideoView = findViewById(R.id.learn_video_view);
+
+        TextView tvDancer = findViewById(R.id.learn_tv_dancer);
+        tvDancer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playVideo();
+            }
+        });
+
+        /*tvDancer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (recording) {
+                    mediaRecorder.stop();
+                    mediaRecorder.release();
+                    camera.lock();
+                    recording = false;
+                } else {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                mediaRecorder = new MediaRecorder();
+                                camera.unlock();
+                                mediaRecorder.setCamera(camera);
+                                mediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+                                mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+                                mediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_720P));
+                                mediaRecorder.setOrientationHint(90);
+                                //mediaRecorder.setOutputFile("/sdcard/test.mp4");
+                                mediaRecorder.setPreviewDisplay(surfaceHolder.getSurface());
+                                mediaRecorder.prepare();
+                                mediaRecorder.start();
+                                recording = true;
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                mediaRecorder.release();
+                            }
+                        }
+                    });
+                }
+            }
+        });*/
+
+    }
+
+    public void playVideo() {
+        String path = "android.resource://org.evolution.dancingstar/"+R.raw.dance_example;
+
+        Uri uri = Uri.parse(path);
+
+        mVideoView.setVideoURI(uri);
+        mVideoView.start();
     }
 }
