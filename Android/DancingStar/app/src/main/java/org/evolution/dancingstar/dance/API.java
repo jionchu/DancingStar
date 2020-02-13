0abc6b6d670d15b7b5435e7ab73f4c23aafd668f
박유진 <yoo@bag-yujin-ui-MacBook-Pro.local>
2020-02-13 15:20:23 +0900

    API class

---
+++ Android/DancingStar/app/src/main/java/org/evolution/dancingstar/dance/API.java
@@ -0,0 +1,90 @@
+package org.evolution.dancingstar.dance;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class API {
+    public static void main(String[] args) {
+
+        StringBuffer reqStr = new StringBuffer();
+
+        String clientId = "j3inr039ff";
+        String clientSecret = "ZFzG0MccfiyCXkWuhsBUehrIH38uA8v1FMLN0gxB";
+
+        try {
+            String paramName = "image"; // 파라미터명은 image로 지정
+            String imgFile = "이미지 파일 경로 ";
+            File uploadFile = new File(imgFile);
+
+            // 사람 인식
+            String apiURL = "https://naveropenapi.apigw.ntruss.com/vision-pose/v1/estimate";
+
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setUseCaches(false);
+            con.setDoOutput(true);
+            con.setDoInput(true);
+
+
+            // multipart request
+            String boundary = "---" + System.currentTimeMillis() + "---";
+            con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+            con.setRequestProperty("X-NCP-APIGW-API-KEY-ID", clientId);
+            con.setRequestProperty("X-NCP-APIGW-API-KEY", clientSecret);
+            OutputStream outputStream = con.getOutputStream();
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream, "UTF-8"), true);
+            String LINE_FEED = "\r\n";
+
+
+
+            // file 추가
+            String fileName = uploadFile.getName();
+            writer.append("--" + boundary).append(LINE_FEED);
+            writer.append("Content-Disposition: form-data; name=\"" + paramName + "\"; filename=\"" + fileName + "\"").append(LINE_FEED);
+            writer.append("Content-Type: "  + URLConnection.guessContentTypeFromName(fileName)).append(LINE_FEED);
+            writer.append(LINE_FEED);
+            writer.flush();
+            FileInputStream inputStream = new FileInputStream(uploadFile);
+            byte[] buffer = new byte[4096];
+            int bytesRead = -1;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            outputStream.flush();
+            inputStream.close();
+            writer.append(LINE_FEED).flush();
+            writer.append("--" + boundary + "--").append(LINE_FEED);
+            writer.close();
+            BufferedReader br = null;
+            int responseCode = con.getResponseCode();
+
+            if(responseCode==200) { // 정상 호출
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {  // 오류 발생
+                System.out.println("error!!!!!!! responseCode= " + responseCode);
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            }
+            String inputLine;
+            if(br != null) {
+                StringBuffer response = new StringBuffer();
+                while ((inputLine = br.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                br.close();
+                System.out.println(response.toString());
+            } else {
+                System.out.println("error !!!");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
