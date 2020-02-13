dad835d27cfb19e41828ccf79cac69b8e9ca652b
mjung1798 <mor2222@naver.com>
2020-02-13 23:01:21 +0900

    api call

--- Backend/dancing-star/src/main/java/com/jyami/dancingstar/controller/AdminController.java
+++ Backend/dancing-star/src/main/java/com/jyami/dancingstar/controller/AdminController.java
@@ -1,27 +1,14 @@
 package com.jyami.dancingstar.controller;
 
 import com.jyami.dancingstar.dto.admin.SaveDanceReqDto;
+import com.jyami.dancingstar.service.PoseEstimationAPIService;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.disk.DiskFileItem;
 
-import org.apache.commons.io.IOUtils;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
-
-import java.io.*;
-import java.net.URL;
-import java.nio.file.Files;
-import java.util.Map;
 
 /**
  * Created by jyami on 2020/02/11
@@ -30,7 +17,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class AdminController {
 
-    private final RestTemplate restTemplate;
+    private final PoseEstimationAPIService poseEstimationAPIService;
 
     @PostMapping("dacing/save")
     public ResponseEntity saveServiceDancingVideo(@RequestBody SaveDanceReqDto saveDanceReqDto){
@@ -38,32 +25,8 @@ public class AdminController {
     }
 
     @GetMapping("test")
-    public ResponseEntity getPose() throws IOException {
-
-        HttpHeaders httpHeaders = new HttpHeaders();
-
-        URL resource = this.getClass().getClassLoader().getResource("static/dance.jpeg");
-        byte[] fileContent = Files.readAllBytes(new File(resource.getFile()).toPath());
-
-        MultiValueMap<String, String> fileMap = new LinkedMultiValueMap<>();
-        ContentDisposition  contentDisposition = ContentDisposition.builder("form-data")
-                .name("file")
-                .filename("dance.jpeg")
-                .build();
-
-        fileMap.add(HttpHeaders.CONTENT_DISPOSITION, contentDisposition.toString());
-        fileMap.add(HttpHeaders.CONTENT_TYPE, "image/jpeg");
-        HttpEntity<byte[]> entity = new HttpEntity<>(fileContent, fileMap);
-        MultiValueMap<String, Object> bodyMap = new LinkedMultiValueMap<>();
-        bodyMap.add("image", entity);
-
-        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(bodyMap, httpHeaders);
-
-        restTemplate.postForEntity(
-                "https://naveropenapi.apigw.ntruss.com/vision-pose/v1/estimate",
-                requestEntity,
-                Map.class);
-
-        return ResponseEntity.ok().build();
+    public ResponseEntity getPose() {
+        String s = poseEstimationAPIService.callAPI("static/dance.jpeg");
+        return ResponseEntity.ok(s);
     }
 }
